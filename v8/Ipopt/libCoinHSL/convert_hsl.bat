aab808c8922c1d628436769d9493d83d979f8277
andreasw <andreasw@c646ee74-6614-0410-ae91-f49df8eabe7c>
2007-06-15 16:54:36 +0000

    More changes for MSVC
    git-svn-id: https://projects.coin-or.org/svn/MSVisualStudio/trunk@117 c646ee74-6614-0410-ae91-f49df8eabe7c

--- v8/Ipopt/libCoinHSL/convert_hsl.bat
+++ v8/Ipopt/libCoinHSL/convert_hsl.bat
@@ -5,7 +5,7 @@ rem This file is distributed under the Common Public License.
 rem
 rem Author:   Andreas Waechter              IBM     2007-06-14
 
-@rem $Id: Makefile.am 735 2006-06-04 06:10:05Z andreasw $
+@rem $Id$
 
 f2c ..\..\..\..\ThirdParty\HSL\ma27ad.f
 f2c ..\..\..\..\ThirdParty\HSL\mc19ad.f
